ffe5f21c0531deda2ff38b846e12f9b878604400
markjasonespelita <markjasonespelita02@gmail.com>
2026-05-30 09:47:59 +0800

    feat: added main.c - edit the file based on the latest main.c test.

--- playground/main.c
+++ playground/main.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-#include "helpers/helperString.h"
-#include "helpers/helperInput.h"
+
+#include "helpers/headers/helperString.h"
+#include "helpers/headers/helperInput.h"
+#include "helpers/headers/helperCommand.h"
+#include "helpers/headers/helperReadFile.h"
 
 int main()
 {
-    char name[100];
-    int age;
-
-    printf("Enter your name: ");
-    helperString(name, 100);
-
-    printf("Enter your age: ");
-    age = helperInt();
-
-    printf("Your name is %s and your age is %d.\n", name, age);
-
-    for(int x = 1; x <= age; x++) {
-        printf("%d\n", x);
-        sleep(1);
-    }
+    helperReadFile("README.md");
 
     return 0;
 }
