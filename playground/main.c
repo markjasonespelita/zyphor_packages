965007db69d5bd3de7092e775d476a69abd06b65
markjasonespelita <markjasonespelita02@gmail.com>
2026-06-01 14:07:23 +0800

    feat: reverted the main.c to it's default playground.

--- playground/main.c
+++ playground/main.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
-#include "helpers/headers/helperCommand.h"
-
-typedef struct {
-    char name[100];
-    int age;
-} User;
+#include <stdlib.h>
 
 int main()
 {
-
-    User users[] = {
-        {"Mark", 32},
-        {"Jason", 22}
-    };
-
-    int size = sizeof(users) / sizeof(users[0]);
-
-    for(int i = 0; i < size; i++) {
-        printf("Name: %s\n", users[i].name);
-        printf("Age: %d\n\n", users[i].age);
-    }
 
     return 0;
 }
